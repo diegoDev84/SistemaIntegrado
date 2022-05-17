423fd786dfc37fa97dc953b85c663093f7164637
diegoDev84 <99288906+diegoDev84@users.noreply.github.com>
2022-05-17 15:41:54 -0300

    Update PedidoRequest.cs

--- SistemaBalcao/Requests/PedidoRequest.cs
+++ SistemaBalcao/Requests/PedidoRequest.cs
@@ -19,7 +19,6 @@ namespace SistemaBalcao.Requests
             var lista = JsonConvert.DeserializeObject<List<Pedido>>(json);
             return lista;
         }
-        
         public static async Task NovoPedido(Pedido pedido)
         {
             var httpClient = new HttpClient();
@@ -27,7 +26,6 @@ namespace SistemaBalcao.Requests
             var content = ToRequest(pedido);
             var response = await httpClient.PostAsync(requestUri: "https://dragon-api.herokuapp.com/pedido", content);
         }
-
         public static async Task AtualizarPedido(Pedido pedido)
         {
             var httpClient = new HttpClient();
@@ -35,14 +33,12 @@ namespace SistemaBalcao.Requests
             var content = ToRequest(pedido);
             var response = await httpClient.PutAsync(requestUri: $"https://dragon-api.herokuapp.com/pedido/{pedido.Id}", content);
         }
-
         public static async Task DeletarPedido(Pedido pedido)
         {
             var httpClient = new HttpClient();
             var uri = $"https://dragon-api.herokuapp.com/pedido/{pedido.Id}";
             var response = await httpClient.DeleteAsync(uri);
         }
-
         public static StringContent ToRequest(object obj)
         {
             var json = JsonConvert.SerializeObject(obj);
